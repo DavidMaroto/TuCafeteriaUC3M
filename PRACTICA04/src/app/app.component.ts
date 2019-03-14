285f66e7d8bd20c9034346e3475dce768cc6f68c
Eduardo Herrera <eduxtreme16@gmail.com>
2019-03-14 23:44:11 +0100

    P04

---
+++ PRACTICA04/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { LlamadaPage } from '../pages/llamada/llamada';
+import { NuevoPage } from '../pages/nuevo/nuevo';
+import { InfoPage } from '../pages/info/info';
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = HomePage;
+
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Llamada', component: LlamadaPage },
+      { title: 'Agregar', component: NuevoPage },
+      { title: 'Version', component: InfoPage },
+
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) { // Esta es la función que abre mis páginas desde el menú
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
